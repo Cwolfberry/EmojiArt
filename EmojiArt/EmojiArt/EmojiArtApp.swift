c96d4ad640f8fc8acbcf9e58e191e8fe0ba6db94
avalan.zhang <>
2023-11-08 16:51:29 +0800

    init project.

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by avalan.zhang on 2023/11/8.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiArt/EmojiArt/EmojiArtModel.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by avalan.zhang on 2023/11/8.
+//
+
+import Foundation
+
+struct EmojiArtModel {
+    
+    
+    
+    
+    
+}
